b88960108b56b30e45e5e0bbc92433c47ddcdc08
Iain Moncrief <Iainmoncrief@gmail.com>
2018-12-12 11:59:21 -1000

    views are now stored in memory

--- app/server.js
+++ app/server.js
@@ -2,17 +2,24 @@ const http = require('http');
 const util = require("util");
 const ejs = require('ejs');
 const fs = require('fs');
+const Func = require('../lib/Func.js');
 
 class Server {
     static init() {
+        Server.views.main = fs.readFileSync('./app/views/main.ejs', 'utf8');
+        Server.views.error = fs.readFileSync('./app/views/404.ejs', 'utf8');
 
+        this.httpserver = http.createServer(function (req, res) {
+            let response = Server.router(req);
+            res.writeHead(response.code);
+            res.end(response.content);
+        });
     }
 
     static view(path) {
-        let template = ejs.compile(fs.readFileSync('./app/views/' + path, 'utf8'), {hello : 'wow'});
-        return template({hello : 'wow'});
+        let template = ejs.compile(fs.readFileSync('./app/views/' + path, 'utf8'), {});
+        return template({code : 200});
     }
-
 
     static router(request) {
         var response = {};
@@ -20,12 +27,11 @@ class Server {
         switch (request.method) {
             case 'GET':
                 let urlParams = request.url.split('/');
-                console.log(urlParams);
                 if (urlParams[1] === 'remote') {
                     response = Server.routes[0].controller(request);
                 } else {
                     response.code = 404;
-                    response.content = '';
+                    response.content = ejs.compile(Server.views.error,{})({code : 404});
                 }
                 break;
 
@@ -34,17 +40,11 @@ class Server {
                 response.content = 'none';
         }
 
-        //console.log(request.method);
-
         return response;
     }
 
     static start() {
-        http.createServer(function (req, res) {
-            let response = Server.router(req);
-            res.writeHead(response.code);
-            res.end(response.content);
-        }).listen(9000);
+        Server.httpserver.listen(9000);
     }
 }
 
@@ -52,7 +52,7 @@ Server.controllers = {
     remote : function (request) {
         return {
             code : 200,
-            content : Server.view('main.ejs')
+            content : ejs.compile(Server.views.main,{})({code : 200})
         }
     }
 };
@@ -64,5 +64,7 @@ Server.routes = [
         controller : Server.controllers.remote
     }
 ];
+
+Server.views = {};
 
 module.exports = Server;
