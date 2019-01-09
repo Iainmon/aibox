c35a6c2653fed9955e9f6461135e9eabb7992b26
Iain Moncrief <Iainmoncrief@gmail.com>
2019-01-09 10:19:14 -1000

    iterating through actions work in C++

--- arduino/Aibox.h
+++ arduino/Aibox.h
@@ -1,15 +1,36 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
+#ifndef AiboxAction_h
+#define AiboxAction_h
+
+class AiBoxAction {
+  public:
+    AiBoxAction(std::function< void() > functocall, std::string actionId);
+    void callAction();
+    void getActionId();
+
+  private:
+    std::function< void() > _functocall;
+    std::string _actionId;
+};
+#endif
+
+
 #ifndef Aibox_h
 #define Aibox_h
 
-class Aibox
-{
+class Aibox {
   public:
     Aibox(int pin);
     std::string dot();
-    void dash();
+    void registerAction(std::string actionId, std::function< void() > functocall);
+    void fireAll();
   private:
     int _pin;
+    std::vector< AiBoxAction > _registeredActions;
 };
+
 #endif
+
--- arduino/aibox.cpp
+++ arduino/aibox.cpp
@@ -4,6 +4,15 @@
 
 using namespace std;
 
+AiBoxAction::AiBoxAction(std::function< void() > functocall, std::string actionId) {
+    _functocall = functocall;
+    _actionId = actionId;
+}
+
+void AiBoxAction::callAction() {
+    _functocall();
+}
+
 std::string Aibox::dot()
 {
     return "hello";
@@ -12,10 +21,23 @@ Aibox::Aibox(int pin) {
     _pin = pin;
 }
 
+void Aibox::registerAction(std::string actionId, std::function< void() > functocall) {
+    _registeredActions.push_back(AiBoxAction(functocall, actionId));
+}
+
+void Aibox::fireAll() {
+    for(std::size_t i = 0; i < _registeredActions.size(); ++i) {
+        _registeredActions[i].callAction();
+    }
+}
+
+void myFunction() {
+    cout << "hello";
+}
+
 int main() {
     Aibox aibox(13);
-    for (int i = 0; i < 100; i++) {
-        cout << aibox.dot();
-    }
+    aibox.registerAction("function1", myFunction);
+    aibox.fireAll();
     return 0;
 }
